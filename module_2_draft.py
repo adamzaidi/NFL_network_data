da943ac2f65dfb1f1c1cf5bfda451c6571ce80ce
Adam Zaidi <93174158+adamzaidi@users.noreply.github.com>
2025-03-07 22:45:47 -0500

    most recent update

--- module_2_draft.py
+++ module_2_draft.py
@@ -3,33 +3,28 @@ from bs4 import BeautifulSoup
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
-import tweepy
-
-# Twitter API credentials (You need to replace these with your own keys)
-API_KEY = 'your_api_key'
-API_SECRET_KEY = 'your_api_secret_key'
-ACCESS_TOKEN = 'your_access_token'
-ACCESS_TOKEN_SECRET = 'your_access_token_secret'
-
-# Authenticate to Twitter API
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # Function to scrape hyperlinks from a given website
 def scrape_links(url):
     links = set()
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for link in soup.find_all('a', href=True):
-            href = link['href']
-            if href.startswith('http'):
-                links.add(href)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for link in soup.find_all('a', href=True):
+                href = link['href']
+                if href.startswith('http'):
+                    links.add(href)
+    except requests.RequestException as e:
+        print(f"Error accessing {url}: {e}")
     return links
 
 # Define key NFL-related websites to scrape
-nfl_sites = ['https://www.nfl.com', 'https://www.espn.com/nfl/', 'https://www.bleacherreport.com/nfl']
+nfl_sites = [
+    'https://www.nfl.com',
+    'https://www.espn.com/nfl/',
+    'https://www.bleacherreport.com/nfl'
+]
 network_data = []
 
 # Scraping links from NFL-related websites
@@ -37,20 +32,6 @@ for site in nfl_sites:
     linked_sites = scrape_links(site)
     for linked_site in linked_sites:
         network_data.append((site, linked_site))
-
-# Fetching Twitter data (mentions of @NFL, retweets, and links shared)
-def fetch_twitter_links(query, count=100):
-    tweets = api.search_tweets(q=query, count=count, tweet_mode='extended')
-    twitter_links = []
-    for tweet in tweets:
-        if 'urls' in tweet.entities:
-            for url in tweet.entities['urls']:
-                twitter_links.append((f'Twitter-{tweet.user.screen_name}', url['expanded_url']))
-    return twitter_links
-
-# Collect Twitter data
-nfl_twitter_links = fetch_twitter_links("@NFL", 200)
-network_data.extend(nfl_twitter_links)
 
 # Create a directed graph
 G = nx.DiGraph()
